2dfc81af9e6d7154180ade63637e23a11ef6afbc
joutwater <54482975+joutwater@users.noreply.github.com>
2019-11-03 13:46:04 -0500

    Update week09_b.js

--- week09/week09_b.js
+++ week09/week09_b.js
@@ -12,78 +12,66 @@ var access_token = process.env.PHOTON_TOKEN;
 var particle_variable = 'tempsensor';
 var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 
-// // AWS RDS POSTGRESQL INSTANCE
-// // var db_credentials = new Object();
-// // db_credentials.user = 'aaron';
-// // db_credentials.host = process.env.AWSRDS_EP;
-// // db_credentials.database = 'mydb';
-// // db_credentials.password = process.env.AWSRDS_PW;
-// // db_credentials.port = 5432;
-
-// var index = 0;
-// var fns = '/home/ec2-user/environment/data/temp.txt'
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'jcoutwater';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'cook';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
 
 var data = {};
+var index = 0;
 
 var getAndWriteData = function() {
     
     // Make request to the Particle API to get sensor values
     request(device_url, function(error, response, body) {
         
-        // Store sensor value(s) in a variable
+        // Store sensor value(s) in variables
         var parseJson = JSON.parse(body);
+        var rawTime = parseJson.coreInfo.last_heard.split('T');
+        var splitTime = rawTime[0].split(/-/);
         
-        data.temperature = parseFloat(parseJson.result);
+        data.id = index;
+        data.year = splitTime[0];
+        data.month = splitTime[1];
+        data.day = splitTime[2];
+        data.time = rawTime[1].slice(0,5);
+        data.rawStamp = rawTime[1];
+        data.temperature = parseJson.result;
+        
+        index++;
+        
+        // console.log(data);
 
-        
-        // var temperature = {};
-        // // data.hour = 12;
-        // temperature = parseFloat(parseJson.result);
-        
-        // data.push(temperature);
-        
-        // var testArray = [];
-        
-        // var sv = {};
-        
-        // var tempValue = {};
-        
-        // tempValue = body.sv.result;
-        
-        // testArray.push(tempValue);
-        
-        // console.log(sv);
-        
-        // console.log("hello");
 
-        
-        // Convert 1/0 to TRUE/FALSE for the Postgres INSERT INTO statement
-        // var sv_mod; 
-        // if (sv == 1) {
-        //     sv_mod = "TRUE";
-        // }
-        // else if (sv == 0) {
-        //     sv_mod = "FALSE";
-        // }
+        // Connect to the AWS RDS Postgres database
+        const client = new Client(db_credentials);
+        client.connect();
 
-        // // Connect to the AWS RDS Postgres database
-        // const client = new Client(db_credentials);
-        // client.connect();
+        // Construct a SQL statement to insert sensor values into a table
+        var thisQuery = "INSERT INTO sensorData VALUES ('" + data.id + "',\
+                                                        '" + data.year + "',\
+                                                        '" + data.month + "',\
+                                                        '" + data.day + "',\
+                                                        '" + data.time + "',\
+                                                        '" + data.rawStamp + "',\
+                                                        '" + data.temperature + "');";
+        console.log(thisQuery); // for debugging
 
-        // // Construct a SQL statement to insert sensor values into a table
-        // var thisQuery = "INSERT INTO sensorData VALUES (" + sv_mod + ", DEFAULT);";
-        // console.log(thisQuery); // for debugging
-
-        // // Connect to the AWS RDS Postgres database and insert a new row of sensor values
-        // client.query(thisQuery, (err, res) => {
-        //     console.log(err, res);
-        //     client.end();
-        // });
+        // Connect to the AWS RDS Postgres database and insert a new row of sensor values
+        client.query(thisQuery, (err, res) => {
+            console.log(err, res);
+            client.end();
+        });
     });
 };
 
-console.log(data);
+// check function discretely
+// getAndWriteData();
+// console.log(data);
 
 
 // write a new row of sensor data every five minutes
-// setInterval(getAndWriteData, 300000);
+setInterval(getAndWriteData, 300000);
